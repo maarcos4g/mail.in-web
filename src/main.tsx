4bcc85ad0095bbef5e426973e77d161e91676111
maarcos4g <marcospaullo552@gmail.com>
2025-01-27 10:52:48 -0300

    fix: rename app file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './app.tsx'
+import { App } from './App'
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/pt-br";
